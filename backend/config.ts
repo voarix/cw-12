f2962cea1fe956550194e15c0f82ec40a8b1369e
Kachkynov Alikhan <t8t228@gmail.com>
2025-05-31 11:27:52 +0600

    [api] initialization

---
+++ backend/config.ts
@@ -0,0 +1,15 @@
+import path from "path";
+
+const rootPath = __dirname;
+
+const config = {
+  rootPath,
+  google: {
+    clientId: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  },
+  publicPath: path.join(rootPath, "public"),
+  db: "mongodb://localhost/cocktails",
+};
+
+export default config;
---
+++ backend/index.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+import express from "express";
+import cors from "cors";
+import config from "./config";
+import cookieParser from "cookie-parser";
+
+const app = express();
+const port = 8000;
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
+
+app.use(cookieParser());
+app.use(express.static("public"));
+app.use(express.json());
+
+const run = async () => {
+  await mongoose.connect(config.db);
+
+  app.listen(port, () => {
+    console.log(`Server started on http://localhost:${port}`);
+  });
+
+  process.on("exit", () => {
+    mongoose.disconnect();
+  });
+};
+
+run().catch(console.error);
